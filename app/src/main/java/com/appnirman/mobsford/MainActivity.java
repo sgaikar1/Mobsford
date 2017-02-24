1a2c47f8f5ec049090605458adbda1fa5d08815e
sgaikar1 <santoshgaikar1@gmail.com>
2017-02-24 12:55:06 +0530

    initial commit

--- app/src/main/java/com/appnirman/mobsford/MainActivity.java
+++ app/src/main/java/com/appnirman/mobsford/MainActivity.java
@@ -1,8 +1,12 @@
 package com.appnirman.mobsford;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +16,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView mRecyclerView;
+    private ProgressBar progressBar;
+    private String TAG=MainActivity.class.getSimpleName();
+    private ArrayList<FeedItem> feedsList;
+    private MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +62,80 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        String url = "http://stacktips.com/?json=get_category_posts&slug=news&count=30";
+        new DownloadTask().execute(url);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            Integer result = 0;
+            HttpURLConnection urlConnection;
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                int statusCode = urlConnection.getResponseCode();
+
+                // 200 represents HTTP OK
+                if (statusCode == 200) {
+                    BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        response.append(line);
+                    }
+                    parseResult(response.toString());
+                    result = 1; // Successful
+                } else {
+                    result = 0; //"Failed to fetch data!";
+                }
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+            return result; //"Failed to fetch data!";
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            progressBar.setVisibility(View.GONE);
+
+            if (result == 1) {
+                adapter = new MyRecyclerViewAdapter(MainActivity.this, feedsList);
+                mRecyclerView.setAdapter(adapter);
+            } else {
+                Toast.makeText(MainActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void parseResult(String result) {
+        try {
+            JSONObject response = new JSONObject(result);
+            JSONArray posts = response.optJSONArray("posts");
+            feedsList = new ArrayList<>();
+
+            for (int i = 0; i < posts.length(); i++) {
+                JSONObject post = posts.optJSONObject(i);
+                FeedItem item = new FeedItem();
+                item.setTitle(post.optString("title"));
+                item.setThumbnail(post.optString("thumbnail"));
+                feedsList.add(item);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/appnirman/mobsford/MyRecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.appnirman.mobsford;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by santosh on 24/2/17.
+ */
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.CustomViewHolder> {
+    private List<FeedItem> feedItemList;
+    private Context mContext;
+
+    public MyRecyclerViewAdapter(Context context, List<FeedItem> feedItemList) {
+        this.feedItemList = feedItemList;
+        this.mContext = context;
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_row, null);
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder customViewHolder, int i) {
+        FeedItem feedItem = feedItemList.get(i);
+
+        //Render image using Picasso library
+        if (!TextUtils.isEmpty(feedItem.getThumbnail())) {
+            Picasso.with(mContext).load(feedItem.getThumbnail())
+                    .error(R.mipmap.ic_launcher)
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(customViewHolder.imageView);
+        }
+
+        //Setting text view title
+        customViewHolder.textView.setText(Html.fromHtml(feedItem.getTitle()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != feedItemList ? feedItemList.size() : 0);
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+        protected ImageView imageView;
+        protected TextView textView;
+
+        public CustomViewHolder(View view) {
+            super(view);
+            this.imageView = (ImageView) view.findViewById(R.id.thumbnail);
+            this.textView = (TextView) view.findViewById(R.id.title);
+        }
+    }
+}
